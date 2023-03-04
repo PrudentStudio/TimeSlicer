0be43a13e6612fb9fd6391c5ae8d57a48e15b18d
navanchauhan <navanchauhan@gmail.com>
2023-03-04 12:50:00 -0700

    Initial Commit

---
+++ TimeSlicer/TimeSlicerApp.swift
@@ -0,0 +1,20 @@
+//
+//  TimeSlicerApp.swift
+//  TimeSlicer
+//
+//  Created by Navan Chauhan on 04/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeSlicerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
